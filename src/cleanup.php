296bc414e938e9dafcd53b9d4b29dbbaddf7459a
Kwanda Silekwa <46996545+kwanda9700@users.noreply.github.com>
2025-07-23 00:35:09 +0200

    Create cleanup.php

---
+++ src/cleanup.php
@@ -0,0 +1,26 @@
+// src/Cleanup.php
+class JsonCleaner {
+    public function clean($input) {
+        $stack = [];
+        $inString = false;
+        $result = '';
+
+        foreach (str_split($input) as $char) {
+            if ($char === '"' && (empty($stack) || end($stack) !== '\\')) {
+                $inString = !$inString;
+            }
+
+            if (!$inString && ($char === '}' || $char === ']')) {
+                if (!empty($stack)) array_pop($stack);
+            }
+
+            $result .= $char;
+
+            if (!$inString && ($char === '{' || $char === '[')) {
+                $stack[] = $char;
+            }
+        }
+
+        return $result;
+    }
+}
